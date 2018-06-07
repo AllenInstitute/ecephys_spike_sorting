4d89fdda14d0f615d20398284a573010967a4bf6
Josh Siegle <joshs@alleninstitute.org>
2018-06-07 15:51:00 -0700

    Update scripts

--- ecephys_spike_sorting/scripts/compute_offset_and_surface_channel.py
+++ ecephys_spike_sorting/scripts/compute_offset_and_surface_channel.py
@@ -9,15 +9,32 @@ Created on Thu Sep  7 14:51:18 2017
 import glob    
 import json
 import os
+from scipy.optimize import minimize
+from scipy import signal
+from scipy.ndimage.filters import gaussian_filter1d
+import numpy as np
+import matplotlib.pyplot as plt
 
-dataFolder = '/mnt/md0/data/mouse380483/probeE'
+dataFolder = r'E:\\704514354_380485_20180601_probeF_sorted\\'
 
-ap_directory = glob.glob(dataFolder + '/continuous/Neuropix*.0')[0]
-lfp_directory = glob.glob(dataFolder + '/continuous/Neuropix*.1')[0]
+f1 = dataFolder + os.path.join('continuous','Neuropix*.0')
+f2 = dataFolder + os.path.join('continuous','Neuropix*.1')
+
+
+ap_directory = glob.glob(f1)[0]
+lfp_directory = glob.glob(f2)[0]
+
+print(ap_directory)
+print(lfp_directory)
+
+hi_noise_thresh = 26
+lo_noise_thresh = 3
+lfp_noise_thresh = -1.0
+surface_ch_thresh = -0.05
 
 # %%
 
-from scipy.optimize import minimize
+
 
 def rms(data):
     return np.power(np.mean(np.power(data,2)),0.5)
@@ -29,116 +46,6 @@ def func(x, channel, M):
     
     return error
 
-# %%
-  
-output_file = os.path.join(dataFolder, 'probe_info.json')
-
-numChannels = 384
-
-offsets = np.zeros((numChannels,), dtype = 'int16)
-rms_noise = np.zeros((numChannels,), dtype='int16')
-lfp_power = np.zeros((numChannels,), dtype = 'float32')
-
-spikes_file = os.path.join(ap_directory, 'continuous.dat')
-lfp_file = os.path.join(lfp_directory, 'continuous.dat')
-
-# %%
-
-from scipy import signal
-
-rawDataAp = np.memmap(spikes_file, dtype='int16', mode='r')
-dataAp = np.reshape(rawDataAp, (int(rawDataAp.size/numChannels), numChannels))
-
-mask_chans = np.array([37, 76, 113, 152, 189, 228, 265, 304, 341, 380]) - 1
-
-start_time = 30000*10
-recording_time = 30000
-median_subtr = np.zeros((recording_time,numChannels))
-
-offsets = np.zeros((numChannels,))
-
-# 1. cycle through to find median offset
-for ch in range(0,numChannels,1): #
-    
-    channel = dataAp[start_time:start_time+recording_time,ch].astype('float32')
-    #a
-    offsets[ch] = np.nanmedian(channel).astype('int16')
-
-    median_subtr[:,ch] = channel - offsets[ch]
-    
-    f,Pxx = signal.welch(median_subtr[:2500,ch],fs=30000,nfft=2048)
-    
-    rms_noise[ch] = rms(median_subtr[:,ch]).astype('int16')
-    lfp_power[ch] = np.log10(Pxx[7])
-    
-    
-# %%
-    
-excluded_chans1 = np.where(rms_noise > 300)[0]
-excluded_chans2 = np.where(rms_noise < 45)[0]
-    
-mask_chans2 = np.concatenate((mask_chans, excluded_chans1, excluded_chans2))
-
-
-# %%
-
-from scipy.ndimage.filters import gaussian_filter1d
-
-rawDataLfp = np.memmap(lfp_file, dtype='int16', mode='r')
-dataLfp = np.reshape(rawDataLfp, (int(rawDataLfp.size/numChannels), numChannels))
-
-mask_chans = np.array([37, 76, 113, 152, 189, 228, 265, 304, 341, 380]) - 1
-
-start_time = 2500*10
-recording_time = 2500
-median_subtr = np.zeros((recording_time,numChannels))
-
-# 1. cycle through to find median offset
-for ch in range(0,numChannels,1): #
-    
-    channel = dataLfp[start_time:start_time+recording_time,ch].astype('float32')
-    #a
-    offset = np.nanmedian(channel)
-
-    median_subtr = channel - offset
-    
-    f,Pxx = signal.welch(median_subtr,fs=2500,nfft=2048)
-    lfp_power[ch] = np.log10(Pxx[20])
-    
-lfp_power[mask_chans2] = lfp_power[mask_chans2-1]    
-low_power = np.where(lfp_power < 1.0)[0]
-lfp_power[low_power] = lfp_power[low_power-1]
-lfp_power = gaussian_filter1d(lfp_power,2)
-
-plt.figure(191211)
-plt.clf()
-    
-plt.subplot(1,3,1)
-plt.plot(offsets)
-
-plt.subplot(1,3,2)
-plt.plot(rms_noise)
-
-plt.plot([0,numChannels],[300,300],'--r')
-plt.plot([0,numChannels],[45,45],'--b')
-
-plt.subplot(1,3,3)
-plt.plot(np.diff(lfp_power))
-plt.plot([0,numChannels],[-0.075,-0.075],'--k')
-
-surface_chan = np.max(np.where(np.diff(lfp_power) < -0.075)[0])
-
-mask_chans3 = np.concatenate(mask_chans2, low_power)
-
-mask = np.ones((ch.shape))
-mask = mask > 0
-mask[mask_chans3] = False
-scaling = np.ones((numChannels,))
-channels = np.arange(0,numChannels)
-
-offset = offsets.astype('int')
-
-# %%
 
 def write_probe_json(output_file, channels, offset, scaling, mask, surface_channel):
 
@@ -203,4 +110,129 @@ def median_subtraction(spikes_file, full_mask, offset, scaling, totalChans):
         rawData[start:end] = this_sample
         
     del rawData
+
+
+# %%
+  
+output_file = os.path.join(dataFolder, 'probe_info.json')
+
+numChannels = 384
+
+offsets = np.zeros((numChannels,), dtype = 'int16')
+rms_noise = np.zeros((numChannels,), dtype='int16')
+lfp_power = np.zeros((numChannels,), dtype = 'float32')
+
+spikes_file = os.path.join(ap_directory, 'continuous.dat')
+lfp_file = os.path.join(lfp_directory, 'continuous.dat')
+
+# %%
+
+rawDataAp = np.memmap(spikes_file, dtype='int16', mode='r')
+dataAp = np.reshape(rawDataAp, (int(rawDataAp.size/numChannels), numChannels))
+
+mask_chans = np.array([37, 76, 113, 152, 189, 228, 265, 304, 341, 380]) - 1
+
+start_time = 30000*20
+recording_time = 90000
+median_subtr = np.zeros((recording_time,numChannels))
+
+offsets = np.zeros((numChannels,))
+
+# 1. cycle through to find median offset
+plt.figure(200)
+plt.clf()
+
+for ch in range(0,numChannels,1): #
+    
+    channel = dataAp[start_time:start_time+recording_time,ch].astype('float32')
+    #a
+    offsets[ch] = np.nanmedian(channel).astype('int16')
+
+    median_subtr[:,ch] = channel - offsets[ch]
+    if ch % 10 == 0:
+        plt.plot(median_subtr[:,ch] + ch*10,linewidth=0.5)
+
+    f,Pxx = signal.welch(median_subtr[:2500,ch],fs=30000,nfft=2048)
+    
+    rms_noise[ch] = rms(median_subtr[:,ch]).astype('int16')
+    lfp_power[ch] = np.log10(Pxx[7])
+    
+plt.show()
+    
+# %%
+    
+excluded_chans1 = np.where(rms_noise > hi_noise_thresh)[0]
+excluded_chans2 = np.where(rms_noise < lo_noise_thresh)[0]
+    
+mask_chans2 = np.concatenate((mask_chans, excluded_chans1, excluded_chans2))
+
+
+# %%
+
+rawDataLfp = np.memmap(lfp_file, dtype='int16', mode='r')
+dataLfp = np.reshape(rawDataLfp, (int(rawDataLfp.size/numChannels), numChannels))
+
+mask_chans = np.array([37, 76, 113, 152, 189, 228, 265, 304, 341, 380]) - 1
+
+start_time = 2500*20
+recording_time = 2500*3
+median_subtr = np.zeros((recording_time,numChannels))
+
+# 1. cycle through to find median 
+plt.figure(199)
+plt.clf()
+
+for ch in range(0,numChannels,1): #
+    
+    channel = dataLfp[start_time:start_time+recording_time,ch].astype('float32')
+    #a
+    offset = np.nanmedian(channel)
+
+    median_subtr = channel - offset
+    if ch % 10 == 0:
+        plt.plot(median_subtr + ch*10,linewidth=0.5)
+    
+    f,Pxx = signal.welch(median_subtr,fs=2500,nfft=2048)
+    lfp_power[ch] = np.log10(np.mean(Pxx[80]))
+
+plt.show()
+
+lfp_power[mask_chans2] = lfp_power[mask_chans2-1]    
+lfp_power = signal.detrend(lfp_power)
+low_power = np.where(lfp_power < lfp_noise_thresh)[0]
+lfp_power[low_power] = lfp_power[low_power-1]
+#lfp_power = gaussian_filter1d(lfp_power,5)
+
+plt.figure(191211)
+plt.clf()
+    
+plt.subplot(1,3,1)
+plt.plot(offsets)
+
+plt.subplot(1,3,2)
+plt.plot(rms_noise)
+
+plt.plot([0,numChannels],[hi_noise_thresh,hi_noise_thresh],'--r')
+plt.plot([0,numChannels],[lo_noise_thresh,lo_noise_thresh],'--b')
+
+plt.subplot(1,3,3)
+plt.plot(lfp_power)
+plt.plot([0,numChannels],[surface_ch_thresh,surface_ch_thresh],'--k')
+
+plt.show()
+
+if False:
+    surface_chan = np.max(np.where(np.diff(lfp_power) < surface_ch_thresh)[0])
+
+    print(surface_chan)
+
+    channels = np.arange(0,numChannels)
+    mask = np.ones((channels.shape))
+    mask = mask > 0
+    mask[mask_chans3] = False
+    scaling = np.ones((numChannels,))
+
+    offset = offsets.astype('int')
+
+    # %%
 
--- ecephys_spike_sorting/scripts/test_script.py
+++ ecephys_spike_sorting/scripts/test_script.py
@@ -1,10 +1,11 @@
 import os
 import subprocess
 
-ssds = ['J:\\','J:\\','J:\\','J:\\','K:\\','K:\\','K:\\','K:\\','L:\\','L:\\']
+ssds = ['L:\\','J:\\','J:\\','J:\\','K:\\','K:\\','K:\\','K:\\','L:\\','L:\\']
 
-input_files = ['703270608_380480_20180529_probeD',
-			   '703640904_380483_20180530_probeD',
+input_files = ['704514354_380485_20180601_probeF',
+				'703640904_380483_20180530_probeD',
+			   '703270608_380480_20180529_probeD',
 			   '704166722_380486_20180531_probeD',
 			   '704514354_380485_20180601_probeD',
 			   '703270608_380480_20180529_probeE',
@@ -12,11 +13,12 @@ input_files = ['703270608_380480_20180529_probeD',
 			   '704166722_380486_20180531_probeE',
 			   '704514354_380485_20180601_probeE',
 			   '703640904_380483_20180530_probeF',
-			   '704514354_380485_20180601_probeF']
+			   ]
 
-recordings = ['1','2','3','1','3','1','4','1','2','1']
+recordings = ['1','2','1','3','1','3','1','4','1','2','2']
 
 npx_executable = r'C:\Users\svc_neuropix\Documents\GitHub\npxextractor\Release\NpxExtractor.exe'
+med_sub_executable = r'C:\Users\svc_neuropix\Documents\GitHub\spikebandmediansubtraction\Builds\VisualStudio2013\Release\SpikeBandMedianSubtraction.exe'
 
 for idx, input_file in enumerate(input_files):
 
@@ -30,7 +32,9 @@ for idx, input_file in enumerate(input_files):
 
 		os.mkdir(output_directory)
 
-	subprocess.check_call([npx_executable, npx_file, output_directory])
+	subprocess.check_call([npx_executable, npx_file, output_directory, str(30)])
+
+	stop
 
 	# compute top channel + offsets
 
