e6e5da4a632e22cddf6c14053edba7cdb1eee0d5
jsiegle <joshs@alleninstitute.org>
2018-06-19 15:33:08 -0700

    Add npx extractor module

---
+++ ecephys_spike_sorting/modules/extract_from_npx/_schemas.py
@@ -0,0 +1,22 @@
+from argschema import ArgSchema, ArgSchemaParser 
+from argschema.schemas import DefaultSchema
+from argschema.fields import Nested, InputDir, String, Float, Dict, Int
+ 
+
+class InputParameters(ArgSchema): 
+    npx_file_location = String()
+    oe_settings_file = String()
+    output_file_location = String()
+    executable_file = String()
+    
+
+class OutputSchema(DefaultSchema): 
+    input_parameters = Nested(InputParameters, 
+                              description=("Input parameters the module " 
+                                           "was run with"), 
+                              required=True) 
+ 
+class OutputParameters(OutputSchema): 
+    # Add your output parameters 
+    execution_time = Float()
+    
