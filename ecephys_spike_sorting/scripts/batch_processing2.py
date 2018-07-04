41c315ecb5e283adaced65e4068fe8445d080f11
Josh Siegle <joshs@alleninstitute.org>
2018-07-04 11:06:02 -0700

    Allow batch processing in parallel sub-processes

--- ecephys_spike_sorting/scripts/batch_processing2.py
+++ ecephys_spike_sorting/scripts/batch_processing2.py
@@ -1,13 +1,15 @@
 import subprocess
 import shutil
 import os
+import time
 
 from create_input_json import createInputJson
 
 #import ecephys_spike_sorting.modules.extract_from_npx as extract_from_npx
 
-npx_files = [r'E:\704166722_380486_20180531_probeE\recording1.npx', 
-             r'E:\704166722_380486_20180531_probeD\recording1.npx']
+npx_files = [r'E:\704514354_380485_20180601_probeF\recording1.npx', 
+             r'E:\704514354_380485_20180601_probeE\recording1.npx',
+             r'E:\704514354_380485_20180601_probeD\recording1.npx']
 
 json_directory = r'C:\Users\svc_neuropix\Documents\json_files'
 
@@ -16,31 +18,44 @@ def copy_data_to_backup_drive(info):
 	new_location = os.path.join(r'E:', os.path.basename(extracted_data_location))
 	shutil.move(extracted_data_location, new_location)
 
-for idx, npx_file in enumerate(npx_files):
+modules = ('noise_templates', 'mean_waveforms', 'quality_metrics')
 
-	if idx == 1:
+for module in modules:
 
-		probe_directory = os.path.dirname(npx_file)
-		session_id = os.path.basename(probe_directory)
+	processes = []
 
-		commands = ('noise_templates', 'mean_waveforms', 'quality_metrics') #, 'quality_metrics')
+	for idx, npx_file in enumerate(npx_files):
 
-		for command in commands:
+		if idx != 1:
 
-			input_json = os.path.join(json_directory, session_id + '_' + command + '-input.json')
-			output_json = os.path.join(json_directory, session_id + '_' + command +'-output.json')
+			probe_directory = os.path.dirname(npx_file)
+			session_id = os.path.basename(probe_directory)
+
+			input_json = os.path.join(json_directory, session_id + '_' + module + '-input.json')
+			output_json = os.path.join(json_directory, session_id + '_' + module +'-output.json')
 
 			info = createInputJson(npx_file, input_json)
 
-			command = ["python", "-m", "ecephys_spike_sorting.modules." + command, 
+			command_string = ["python", "-m", "ecephys_spike_sorting.modules." + module, 
 						"--input_json", input_json,
 			            "--output_json", output_json]
 
-			subprocess.check_call(command)
+			print(command_string)
 
-		#copy_data_to_backup_drive(info)
+			# launch sub-processes for each file
+			if module != 'kilosort_helper':
+				processes.append(subprocess.Popen(command_string))
+			else:
+				subprocess.check_call(command)
 
-		#stop
+
+	for p in processes:
+		while p.poll() is None: 
+			time.sleep(0.5)
+
+	#copy_data_to_backup_drive(info)
+
+	#stop
 
 
 
