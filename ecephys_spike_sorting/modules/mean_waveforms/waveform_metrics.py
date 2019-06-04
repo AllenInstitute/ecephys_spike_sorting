5f0052c5fcb5e244c7c338f1f41437db3c08c407
Xiaoxuan Jia <xiaoxuanj@alleninstitute.org>
2019-06-04 14:15:15 -0700

    corrected bug created by detecting peak before trough in the waveform duration

--- ecephys_spike_sorting/modules/mean_waveforms/waveform_metrics.py
+++ ecephys_spike_sorting/modules/mean_waveforms/waveform_metrics.py
@@ -142,7 +142,11 @@ def calculate_waveform_duration(waveform, timestamps):
     trough_idx = np.argmin(waveform)
     peak_idx = np.argmax(waveform)
 
-    duration = np.abs(timestamps[peak_idx] - timestamps[trough_idx])
+    # to avoid detecting peak before trough
+    if waveform[peak_idx] > np.abs(waveform[trough_idx]):
+        duration =  timestamps[peak_idx:][np.where(waveform[peak_idx:]==np.min(waveform[peak_idx:]))[0][0]] - timestamps[peak_idx] 
+    else:
+        duration =  timestamps[trough_idx:][np.where(waveform[trough_idx:]==np.max(waveform[trough_idx:]))[0][0]] - timestamps[trough_idx] 
 
     return duration * 1e3
 
