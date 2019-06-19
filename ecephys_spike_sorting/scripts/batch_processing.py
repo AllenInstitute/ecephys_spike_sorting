080a951e2d87e252088986dfb3d649e8cb6133f5
Josh Siegle <joshs@alleninstitute.org>
2019-06-18 17:47:36 -0700

    Fix typo in batch_processing.py

--- ecephys_spike_sorting/scripts/batch_processing.py
+++ ecephys_spike_sorting/scripts/batch_processing.py
@@ -20,18 +20,18 @@ for directory in sorted_directories:
 
 	info = createInputJson(input_json, kilosort_output_directory=directory)
 
-	modules = [ 'extract_from_npx',
-				'depth_estimation',
-				'median_subtraction',
-				'kilosort_helper',
-				'kilosort_postprocessing',
-				'noise_templates',
-				'mean_waveforms',
-				'quality_metrics']
+	modules = [ #'extract_from_npx',
+				#'depth_estimation',
+				##'median_subtraction',
+				#'kilosort_helper',
+				#'kilosort_postprocessing',
+				#'noise_templates',
+				'mean_waveforms'] #,
+				#'quality_metrics']
 
 	for module in modules:
 
-		command = "python '-W ignore -m ecephys_spike_sorting.modules." + module + " --input_json " + input_json \
+		command = "python -W ignore -m ecephys_spike_sorting.modules." + module + " --input_json " + input_json \
 		          + " --output_json " + output_json
 
 		subprocess.check_call(command.split(' '))
