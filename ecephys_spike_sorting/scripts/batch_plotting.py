3855129747b905d24f2812d7ae00d9419e49dfa7
Josh Siegle <joshs@alleninstitute.org>
2019-07-25 12:29:55 -0700

    Update batch plotting

--- ecephys_spike_sorting/scripts/batch_plotting.py
+++ ecephys_spike_sorting/scripts/batch_plotting.py
@@ -3,11 +3,13 @@ import glob
 
 from ecephys_spike_sorting.common.visualization import plotContinuousFile
 
-mouse = '433891'
+mice = ['434494'] #,'434845','434494','434843']
 
-probe = 'probeB'
+probe = 'probeC'
 
-output_path = r'/mnt/nvme0/continuous_file_qc/after_median_subtraction.png'
-raw_data_file = glob.glob(r'/mnt/sd5.3/RE-SORT/*' + mouse + '*/*' + probe + '*/continuous/Neuropix-3a-100.0/continuous.dat')[0]
+for mouse in mice:
 
-plotContinuousFile(raw_data_file, time_range = [10, 12], output_path=output_path)
+    output_path = r'/mnt/nvme0/continuous_file_qc/' + mouse + '_' + probe + '.png'
+    raw_data_file = glob.glob(r'/mnt/sd5.3/RE-SORT/*' + mouse + '*/*' + probe + '*_2/continuous/Neuropix-3a-100.0/continuous.dat')[0]
+
+    plotContinuousFile(raw_data_file, time_range = [5000, 5002], output_path=output_path)
