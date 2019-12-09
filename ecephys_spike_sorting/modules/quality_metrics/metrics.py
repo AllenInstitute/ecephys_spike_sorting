Fix bug in silhouette score calculation

ecephys_spike_sorting/modules/quality_metrics/metrics.py
@@ -345,7 +345,12 @@ def calculate_silhouette_score(spike_clusters,
                 if len(labels) > 2:
                     SS[i,j] = silhouette_score(X, labels)
 
-    return np.nanmin(SS,0)
+    with warnings.catch_warnings():
+      warnings.simplefilter("ignore")
+      a = np.nanmin(SS, 0)
+      b = np.nanmin(SS, 1)
+
+    return np.array([np.nanmin([a,b]) for a, b in zip(a,b)])
 
 
 def calculate_drift_metrics(spike_times,