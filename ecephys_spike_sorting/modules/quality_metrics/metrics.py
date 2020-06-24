09a7f60413ead33a36ee1e069ad46f848a57e71d
maxym Myroshnychenko <mmyros@gmail.com>
2020-06-24 11:20:54 -0400

    Joblib parallelization of calculate_pc_metrics

--- ecephys_spike_sorting/modules/quality_metrics/metrics.py
+++ ecephys_spike_sorting/modules/quality_metrics/metrics.py
@@ -221,6 +221,74 @@ def calculate_amplitude_cutoff(spike_clusters, amplitudes, total_units):
     return amplitude_cutoffs
 
 
+def calculate_pc_metrics_one_cluster(cluster_peak_channels, idx, cluster_id,cluster_ids,
+                                         half_spread, pc_features, pc_feature_ind,
+                                         spike_clusters, spike_templates,
+                                         max_spikes_for_cluster, max_spikes_for_nn, n_neighbors):
+
+    peak_channel = cluster_peak_channels[idx]
+    num_spikes_in_cluster = np.sum(spike_clusters == cluster_id)
+
+    half_spread_down = peak_channel \
+        if peak_channel < half_spread \
+        else half_spread
+
+    half_spread_up = np.max(pc_feature_ind) - peak_channel \
+        if peak_channel + half_spread > np.max(pc_feature_ind) \
+        else half_spread
+
+    channels_to_use = np.arange(peak_channel - half_spread_down, peak_channel + half_spread_up + 1)
+    units_in_range = cluster_ids[np.isin(cluster_peak_channels, channels_to_use)]
+
+    spike_counts = np.zeros(units_in_range.shape)
+
+    for idx2, cluster_id2 in enumerate(units_in_range):
+        spike_counts[idx2] = np.sum(spike_clusters == cluster_id2)
+
+    if num_spikes_in_cluster > max_spikes_for_cluster:
+        relative_counts = spike_counts / num_spikes_in_cluster * max_spikes_for_cluster
+    else:
+        relative_counts = spike_counts
+
+    all_pcs = np.zeros((0, pc_features.shape[1], channels_to_use.size))
+    all_labels = np.zeros((0,))
+
+    for idx2, cluster_id2 in enumerate(units_in_range):
+
+        subsample = int(relative_counts[idx2])
+
+        pcs = get_unit_pcs(cluster_id2, spike_clusters, spike_templates,
+                           pc_feature_ind, pc_features, channels_to_use,
+                           subsample)
+
+        if pcs is not None and len(pcs.shape) == 3:
+
+            labels = np.ones((pcs.shape[0],)) * cluster_id2
+
+            all_pcs = np.concatenate((all_pcs, pcs),0)
+            all_labels = np.concatenate((all_labels, labels),0)
+
+    all_pcs = np.reshape(all_pcs, (all_pcs.shape[0], pc_features.shape[1]*channels_to_use.size))
+    if ((all_pcs.shape[0] > 10)
+            and (cluster_id in all_labels)
+            and (len(channels_to_use) > 0)):
+        isolation_distance, l_ratio = mahalanobis_metrics(all_pcs, all_labels, cluster_id)
+
+        d_prime = lda_metrics(all_pcs, all_labels, cluster_id)
+
+        nn_hit_rate, nn_miss_rate = nearest_neighbors_metrics(all_pcs, all_labels,
+                                                                             cluster_id,
+                                                                             max_spikes_for_nn,
+                                                                             n_neighbors)
+    else:  # Too few spikes or cluster doesnt exist
+        isolation_distance = np.nan
+        d_prime = np.nan
+        nn_miss_rate = np.nan
+        nn_hit_rate = np.nan
+        l_ratio = np.nan
+    return isolation_distance, d_prime, nn_miss_rate, nn_hit_rate, l_ratio
+
+
 def calculate_pc_metrics(spike_clusters,
                          spike_templates,
                          total_units,
@@ -229,9 +297,22 @@ def calculate_pc_metrics(spike_clusters,
                          num_channels_to_compare,
                          max_spikes_for_cluster,
                          max_spikes_for_nn,
-                         n_neighbors):
+                         n_neighbors,
+                         do_parallel=True):
+    """
 
-    assert(num_channels_to_compare % 2 == 1)
+    :param spike_clusters:
+    :param total_units:
+    :param pc_features:
+    :param pc_feature_ind:
+    :param num_channels_to_compare:
+    :param max_spikes_for_cluster:
+    :param max_spikes_for_nn:
+    :param n_neighbors:
+    :return:
+    """
+
+    assert (num_channels_to_compare % 2 == 1)
     half_spread = int((num_channels_to_compare - 1) / 2)
 
     cluster_ids = np.unique(spike_clusters)
@@ -239,15 +320,10 @@ def calculate_pc_metrics(spike_clusters,
 
     template_peak_channels = np.zeros((len(template_ids),), dtype='uint16')
     cluster_peak_channels = np.zeros((len(cluster_ids),), dtype='uint16')
-    isolation_distances = np.zeros((total_units,))
-    l_ratios = np.zeros((total_units,))
-    d_primes = np.zeros((total_units,))
-    nn_hit_rates = np.zeros((total_units,))
-    nn_miss_rates = np.zeros((total_units,))
 
     for idx, template_id in enumerate(template_ids):
         for_template = np.squeeze(spike_templates == template_id)
-        pc_max = np.argmax(np.mean(pc_features[for_template, 0, :],0))
+        pc_max = np.argmax(np.mean(pc_features[for_template, 0, :], 0))
         template_peak_channels[idx] = pc_feature_ind[template_id, pc_max]
 
     for idx, cluster_id in enumerate(cluster_ids):
@@ -256,72 +332,43 @@ def calculate_pc_metrics(spike_clusters,
         template_positions = np.where(np.isin(template_ids, templates_for_unit))[0]
         cluster_peak_channels[idx] = np.median(template_peak_channels[template_positions])
 
-    for idx, cluster_id in enumerate(cluster_ids):
+    # Loop over clusters:
+    if do_parallel:
+        from joblib import Parallel, delayed
+        meas = Parallel(n_jobs=-1, verbose=3)(  # -1 means use all cores
+            delayed(calculate_pc_metrics_one_cluster)  # Function
+            (cluster_peak_channels, idx, cluster_id, cluster_ids,
+             half_spread, pc_features, pc_feature_ind,
+             spike_clusters, spike_templates,
+             max_spikes_for_cluster, max_spikes_for_nn, n_neighbors
+             )
+            for idx, cluster_id in enumerate(cluster_ids))  # Loop
+    else:
+        from tqdm import tqdm
+        meas = []
+        for idx, cluster_id in tqdm(enumerate(cluster_ids), total=cluster_ids.max(), desc='PC metrics'):  # Loop
+            meas.append(calculate_pc_metrics_one_cluster(  # Function
+                cluster_peak_channels, idx, cluster_id, cluster_ids,
+                half_spread, pc_features, pc_feature_ind,
+                spike_clusters, spike_templates,
+                max_spikes_for_cluster, max_spikes_for_nn, n_neighbors))
 
-        printProgressBar(idx + 1, total_units)
+    # Unpack:
+    isolation_distances = []
+    l_ratios = []
+    d_primes = []
+    nn_hit_rates = []
+    nn_miss_rates = []
+    for mea in meas:
+        isolation_distance, d_prime, nn_miss_rate, nn_hit_rate, l_ratio = mea
+        isolation_distances.append(isolation_distance)
+        d_primes.append(d_prime)
+        nn_miss_rates.append(nn_miss_rate)
+        nn_hit_rates.append(nn_hit_rate)
+        l_ratios.append(l_ratio)
 
-        peak_channel = cluster_peak_channels[idx]
-        num_spikes_in_cluster = np.sum(spike_clusters == cluster_id)
-
-        half_spread_down = peak_channel \
-            if peak_channel < half_spread \
-            else half_spread
-
-        half_spread_up = np.max(pc_feature_ind) - peak_channel \
-            if peak_channel + half_spread > np.max(pc_feature_ind) \
-            else half_spread
-
-        channels_to_use = np.arange(peak_channel - half_spread_down, peak_channel + half_spread_up + 1)
-
-        units_in_range = cluster_ids[np.isin(cluster_peak_channels, channels_to_use)]
-
-        spike_counts = np.zeros(units_in_range.shape)
-
-        for idx2, cluster_id2 in enumerate(units_in_range):
-            spike_counts[idx2] = np.sum(spike_clusters == cluster_id2)
-
-        if num_spikes_in_cluster > max_spikes_for_cluster:
-            relative_counts = spike_counts / num_spikes_in_cluster * max_spikes_for_cluster
-        else:
-            relative_counts = spike_counts
-
-        all_pcs = np.zeros((0, pc_features.shape[1], channels_to_use.size))
-        all_labels = np.zeros((0,))
-
-        for idx2, cluster_id2 in enumerate(units_in_range):
-
-            subsample = int(relative_counts[idx2])
-
-            pcs = get_unit_pcs(cluster_id2, spike_clusters, spike_templates,
-                               pc_feature_ind, pc_features, channels_to_use,
-                               subsample)
-
-            if pcs is not None and len(pcs.shape) == 3:
-
-                labels = np.ones((pcs.shape[0],)) * cluster_id2
-
-                all_pcs = np.concatenate((all_pcs, pcs),0)
-                all_labels = np.concatenate((all_labels, labels),0)
-
-        all_pcs = np.reshape(all_pcs, (all_pcs.shape[0], pc_features.shape[1]*channels_to_use.size))
-
-        if all_pcs.shape[0] > 10:
-
-            isolation_distances[idx], l_ratios[idx] = mahalanobis_metrics(all_pcs, all_labels, cluster_id)
-
-            d_primes[idx] = lda_metrics(all_pcs, all_labels, cluster_id)
-
-            nn_hit_rates[idx], nn_miss_rates[idx] = nearest_neighbors_metrics(all_pcs, all_labels, cluster_id, max_spikes_for_nn, n_neighbors)
-
-        else:
-
-            isolation_distances[idx] = np.nan
-            d_primes[idx] = np.nan
-            nn_hit_rates[idx] = np.nan
-            nn_miss_rates[idx] = np.nan
-
-
-    return isolation_distances, l_ratios, d_primes, nn_hit_rates, nn_miss_rates
+    return (np.array(isolation_distances), np.array(l_ratios), np.array(d_primes),
+            np.array(nn_hit_rates), np.array(nn_miss_rates))
 
 
 def calculate_silhouette_score(spike_clusters,
@@ -763,6 +810,25 @@ def nearest_neighbors_metrics(all_pcs, all_labels, this_unit_id, max_spikes_for_
 # HELPER FUNCTIONS:
 
 # ==========================================================
+
+def features_intersect(pc_feature_ind, these_channels):
+    """
+    # Take only the channels that have calculated features out of the ones we are interested in:
+    # This should reduce the occurence of 'except IndexError' below
+
+    Args:
+        pc_feature_ind
+        these_channels: channels_to_use or units_for_channel
+
+    Returns:
+        channels_to_use: intersect of what's available in PCs and what's needed
+    """
+    intersect = set(pc_feature_ind[these_channels[0], :])  # Initialize
+    for cluster_id2 in these_channels:
+        # Make a running intersect of what is available and what is needed
+        intersect = intersect & set(pc_feature_ind[cluster_id2, :])
+    return np.array(list(intersect))
+
 
 def get_unit_pcs(unit_id,
                  spike_clusters,
